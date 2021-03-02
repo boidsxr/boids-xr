301598f600182f30a9fdc7e4690088a804d13adc
Max Froumentin <github@lapin-bl.eu>
2021-03-02 23:46:54 +0000

    Text for debugging

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from './build/three.module.js';
 
-
 import Stats from './js/stats.module.js';
+
 import { GUI } from './js/dat.gui.module.js';
 
 import { GPUComputationRenderer } from './js/GPUComputationRenderer.js';
@@ -14,72 +14,57 @@ const WIDTH = 32;
 
 const BIRDS = WIDTH * WIDTH;
 
+const showBoids = false;
+
+
 // Custom Geometry - using 3 triangles each. No UVs, no normals currently.
 function BirdGeometry() {
-
   const triangles = BIRDS * 3;
   const points = triangles * 3;
-
   THREE.BufferGeometry.call( this );
-
   const vertices = new THREE.BufferAttribute( new Float32Array( points * 3 ), 3 );
   const birdColors = new THREE.BufferAttribute( new Float32Array( points * 3 ), 3 );
   const references = new THREE.BufferAttribute( new Float32Array( points * 2 ), 2 );
   const birdVertex = new THREE.BufferAttribute( new Float32Array( points ), 1 );
-
   this.setAttribute( 'position', vertices );
   this.setAttribute( 'birdColor', birdColors );
   this.setAttribute( 'reference', references );
   this.setAttribute( 'birdVertex', birdVertex );
 
   // this.setAttribute( 'normal', new Float32Array( points * 3 ), 3 );
-
-
   let v = 0;
-
   function verts_push() {
-
     for ( let i = 0; i < arguments.length; i ++ ) {
-
       vertices.array[ v ++ ] = arguments[ i ];
-
     }
-
   }
 
   const wingsSpan = 20;
-
   for ( let f = 0; f < BIRDS; f ++ ) {
-
     // Body
     verts_push(
       0, - 0, - 20,
       0, 4, - 20,
       0, 0, 30
     );
-
     // Left Wing
     verts_push(
       0, 0, - 15,
       - wingsSpan, 0, 0,
       0, 0, 15
     );
-
     // Right Wing
     verts_push(
       0, 0, 15,
       wingsSpan, 0, 0,
       0, 0, - 15
     );
-
   }
 
   for ( let v = 0; v < triangles * 3; v ++ ) {
-
     const i = ~ ~ ( v / 3 );
     const x = ( i % WIDTH ) / WIDTH;
     const y = ~ ~ ( i / WIDTH ) / WIDTH;
-
     const c = new THREE.Color(
       0x444444 +
         ~ ~ ( v / 9 ) / BIRDS * 0x666666
@@ -93,14 +78,13 @@ function BirdGeometry() {
     references.array[ v * 2 + 1 ] = y;
 
     birdVertex.array[ v ] = v % 9;
-
   }
-
   this.scale( 0.2, 0.2, 0.2 );
-
 }
 
-BirdGeometry.prototype = Object.create( THREE.BufferGeometry.prototype );
+if (showBoids) {
+  BirdGeometry.prototype = Object.create( THREE.BufferGeometry.prototype );
+}
 
 
 let container, stats;
@@ -121,14 +105,45 @@ let positionUniforms;
 let velocityUniforms;
 let birdUniforms;
 
+let font;
+
 init();
+setInterval(() => {
+  const now = new Date();
+  refreshText(now.toString().slice(16,24), font, scene)
+}, 1000);
 animate();
 
-function init() {
 
+let textMesh1;
+function refreshText(text, font, scene) {
+  if (textMesh1) scene.remove(textMesh1);
+  const textGeo = new THREE.TextGeometry(
+    text,
+    {
+      font,
+      size: 70,
+      height: 20,
+    }
+  );
+  textGeo.computeBoundingBox();
+  const textMaterials = [
+    new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } ), // front
+    new THREE.MeshPhongMaterial( { color: 0xffffff } ) // side
+  ];
+  textMesh1 = new THREE.Mesh( textGeo, textMaterials );
+  const centerOffset = - 0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
+  textMesh1.position.x = centerOffset;
+  textMesh1.position.y = 30;
+  textMesh1.position.z = -100;
+  textMesh1.rotation.x = 0;
+  textMesh1.rotation.y = Math.PI * 2;
+  scene.add(textMesh1);
+}
+
+function init() {
   container = document.createElement( 'div' );
   document.body.appendChild( container );
-
 
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 3000 );
   camera.position.z = 350;
@@ -142,7 +157,19 @@ function init() {
   const floorMaterial = new THREE.MeshPhongMaterial( { color: 0x00FF00 } );
   const floor = new THREE.Mesh( floorGeometry, floorMaterial );
   floor.position.y = -300;
+  floor.receiveShadow = true;
   scene.add( floor );
+
+
+  // ==== text
+  const loader = new THREE.FontLoader();
+  loader.load('optimer_bold.typeface.json', response => {
+    font = response;
+//    refreshText('', font, scene);
+  });
+
+
+
 
   const spotLight = new THREE.SpotLight( 0xffffff );
   spotLight.position.set( 100, 1000, 100 );
@@ -152,13 +179,14 @@ function init() {
 
   scene.add( spotLight );
 
-
+  scene.add( new THREE.HemisphereLight( 0x808080, 0x606060 ) );
 
   renderer = new THREE.WebGLRenderer();
   renderer.xr.enabled = true;
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   container.appendChild( renderer.domElement );
+
 
   // VR Button
   document.body.appendChild(VRButton.createButton(renderer));
@@ -188,44 +216,51 @@ function init() {
 
   // end VR stuff
 
-  initComputeRenderer();
+  if (showBoids) {
+    initComputeRenderer();
+  }
 
   stats = new Stats();
   container.appendChild( stats.dom );
 
   container.style.touchAction = 'none';
-  container.addEventListener( 'pointermove', onPointerMove );
+
+  if (showBoids) {
+    container.addEventListener( 'pointermove', onPointerMove );
+  }
 
   window.addEventListener( 'resize', onWindowResize );
 
 
-  const gui = new GUI();
+  let gui;
+  let effectController;
+  if (showBoids) {
+    gui = new GUI();
 
+    effectController = {
+      separation: 20.0,
+      alignment: 20.0,
+      cohesion: 20.0,
+      freedom: 0.75
+    };
 
-  const effectController = {
-    separation: 20.0,
-    alignment: 20.0,
-    cohesion: 20.0,
-    freedom: 0.75
-  };
+    const valuesChanger = function () {
+      velocityUniforms[ 'separationDistance' ].value = effectController.separation;
+      velocityUniforms[ 'alignmentDistance' ].value = effectController.alignment;
+      velocityUniforms[ 'cohesionDistance' ].value = effectController.cohesion;
+      velocityUniforms[ 'freedomFactor' ].value = effectController.freedom;
 
-  const valuesChanger = function () {
+    };
 
-    velocityUniforms[ 'separationDistance' ].value = effectController.separation;
-    velocityUniforms[ 'alignmentDistance' ].value = effectController.alignment;
-    velocityUniforms[ 'cohesionDistance' ].value = effectController.cohesion;
-    velocityUniforms[ 'freedomFactor' ].value = effectController.freedom;
+    valuesChanger();
 
-  };
+    gui.add( effectController, 'separation', 0.0, 100.0, 1.0 ).onChange( valuesChanger );
+    gui.add( effectController, 'alignment', 0.0, 100, 0.001 ).onChange( valuesChanger );
+    gui.add( effectController, 'cohesion', 0.0, 100, 0.025 ).onChange( valuesChanger );
+    gui.close();
 
-  valuesChanger();
-
-  gui.add( effectController, 'separation', 0.0, 100.0, 1.0 ).onChange( valuesChanger );
-  gui.add( effectController, 'alignment', 0.0, 100, 0.001 ).onChange( valuesChanger );
-  gui.add( effectController, 'cohesion', 0.0, 100, 0.025 ).onChange( valuesChanger );
-  gui.close();
-
-  initBirds();
+    initBirds();
+  }
 }
 
 
@@ -357,30 +392,35 @@ renderer.setAnimationLoop(animate);
 
 function render() {
   const now = performance.now();
-  let delta = ( now - last ) / 1000;
 
-  if ( delta > 1 ) delta = 1; // safety cap on large deltas
-  last = now;
 
-  positionUniforms[ 'time' ].value = now;
-  positionUniforms[ 'delta' ].value = delta;
-  velocityUniforms[ 'time' ].value = now;
-  velocityUniforms[ 'delta' ].value = delta;
-  birdUniforms[ 'time' ].value = now;
-  birdUniforms[ 'delta' ].value = delta;
+  if (showBoids) {
+    let delta = ( now - last ) / 1000;
 
-  velocityUniforms[ 'predator' ].value.set( 0.5 * mouseX / windowHalfX, - 0.5 * mouseY / windowHalfY, 0 );
+    if ( delta > 1 ) delta = 1; // safety cap on large deltas
+    last = now;
 
-  mouseX = 10000;
-  mouseY = 10000;
 
-  renderer.xr.enabled = false;
-  gpuCompute.compute();
-  birdUniforms[ 'texturePosition' ].value =
-    gpuCompute.getCurrentRenderTarget( positionVariable ).texture;
-  birdUniforms[ 'textureVelocity' ].value =
-    gpuCompute.getCurrentRenderTarget( velocityVariable ).texture;
-  renderer.xr.enabled = true;
+    positionUniforms[ 'time' ].value = now;
+    positionUniforms[ 'delta' ].value = delta;
+    velocityUniforms[ 'time' ].value = now;
+    velocityUniforms[ 'delta' ].value = delta;
 
+    birdUniforms[ 'time' ].value = now;
+    birdUniforms[ 'delta' ].value = delta;
+
+    velocityUniforms[ 'predator' ].value.set( 0.5 * mouseX / windowHalfX, - 0.5 * mouseY / windowHalfY, 0 );
+
+    mouseX = 10000;
+    mouseY = 10000;
+
+    renderer.xr.enabled = false;
+    gpuCompute.compute();
+    birdUniforms[ 'texturePosition' ].value =
+      gpuCompute.getCurrentRenderTarget( positionVariable ).texture;
+    birdUniforms[ 'textureVelocity' ].value =
+      gpuCompute.getCurrentRenderTarget( velocityVariable ).texture;
+    renderer.xr.enabled = true;
+  }
   renderer.render( scene, camera );
 }
